f78f43c831d934b2505908b42ade2977381c95ce
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-08-10 15:32:14 +0700

    modal region

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -2,12 +2,19 @@
 import { useEffect, useState } from "react";
 import { useSearchParams, usePathname } from "next/navigation";
 import Link from "next/link";
-import { FiAlertTriangle, FiX } from "react-icons/fi";
+import { FiAlertTriangle, FiChevronDown, FiX } from "react-icons/fi";
+import countries from "@/app/constant/Country";
+
+interface Country {
+    name: string;
+    code: string;
+}
 
 function Modal() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const [isClient, setIsClient] = useState(false);
+  const [SelectedCountry, setSelectedCountry] = useState<Country | null>(null);
   const modal = searchParams.get("modal");
 
   useEffect(() => {
@@ -47,17 +54,36 @@ function Modal() {
                 />
               </div>
 
-              <div>
-                <label className="block text-sm font-medium mb-2">Label</label>
-                <select
-                  id="hs-select-label"
-                  className="py-3 px-3 pe-9 block border w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+              {/* dropdown */}
+              <div className="z-10 hs-dropdown relative inline-flex flex-col w-full mt-2">
+                <p className="text-sm font-medium mb-2 text-start">Region</p>
+                <button
+                  id="hs-dropdown-default"
+                  type="button"
+                  className="hs-dropdown-toggle py-3 px-2 inline-flex items-center gap-x-2 w-full text-sm font-medium rounded-lg border border-slate-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
                 >
-                  <option selected={true}>Select Your Region</option>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                </select>
+                  <p className="w-full ml-2 text-start">
+                    {SelectedCountry?.name || "Select Region"}
+                  </p>
+                  <FiChevronDown />
+                </button>
+
+                <div
+                  className="hs-dropdown-menu transition-[opacity,margin] max-h-[300px] overflow-y-scroll duration hs-dropdown-open:opacity-100 opacity-0 hidden bg-white shadow-md rounded-lg p-1 mt-2 after:h-4 after:absolute z-50 after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:start-0 before:w-full"
+                  aria-labelledby="hs-dropdown-default"
+                >
+                  {countries.map((country, index) => (
+                    <a
+                      key={index}
+                      className="flex items-center my-1 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                      href="#"
+                      onClick={() => setSelectedCountry(country)}
+                    >
+                      {country.name}
+                    </a>
+                  ))}
+                  <input type="text" value={SelectedCountry?.code} className="hidden"/>
+                </div>
               </div>
             </form>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import dynamic from "next/dynamic";
+import { FiChevronDown } from "react-icons/fi";
 
 interface UserData {
   id: number;
@@ -16,10 +17,19 @@ interface UserData {
   is_premium?: boolean;
 }
 
+const countries = [
+  "Indonesia",
+  "Singapore",
+  "Malaysia",
+  "Thailand",
+  "Vietnam",
+  "Philippines",
+];
+
 export default function Home() {
     const [userData, setUserData] = useState<UserData | null>(null);
     const [loggedIn, setLoggedIn] = useState(false);
-    
+      const [selectedSize, setSelectedSize] = useState<string | null>(null);
 
     useEffect(() => {
       if (WebApp.initDataUnsafe.user) {
@@ -50,6 +60,35 @@ export default function Home() {
           <div>Loading...</div>
         )}
         <Counter />
+
+        <div className="hs-dropdown relative inline-flex justify-center items-center">
+          <button
+            id="hs-dropdown-default"
+            type="button"
+            className="hs-dropdown-toggle py-2 px-2 inline-flex items-center gap-x-2 min-w-[70%] text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+          >
+            <p className="w-full text-center text-B3">
+              {selectedSize || "Select Region"}
+            </p>
+            <FiChevronDown />
+          </button>
+
+          <div
+            className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden bg-white shadow-md rounded-lg p-1 mt-2 after:h-4 after:absolute z-50 after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:start-0 before:w-full"
+            aria-labelledby="hs-dropdown-default"
+          >
+            {countries.map((country, index) => (
+              <a
+                key={index}
+                className="flex items-center my-1 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                href="#"
+                onClick={() => setSelectedSize(country)}
+              >
+                {country}
+              </a>
+            ))}
+          </div>
+        </div>
       </main>
     </div>
   );

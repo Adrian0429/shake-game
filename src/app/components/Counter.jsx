2b92a0f2207fa9c122f1cd1449054056c9f49ba9
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-08-12 23:46:53 +0700

    bug fix: energy below 0

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect, useRef } from 'react';
 import Shake from 'shake.js';
 import Image from 'next/image';
@@ -12,17 +12,11 @@ const Counter = () => {
     const maxEnergy = 100;
 
     useEffect(() => {
-        if (!permissionGranted){
+        if (!permissionGranted) {
             checkMotionPermission();
-        };
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-    
-    const handleClick = () => {
-        if (count < maxEnergy) {
-            setCount((prevCount) => prevCount + 1);
-        }
-    };
 
     const checkMotionPermission = async () => {
         try {
@@ -38,7 +32,6 @@ const Counter = () => {
             }
         } catch (error) {
             console.error('Error requesting DeviceMotionEvent permission:', error);
-            if (btnRef.current) btnRef.current.style.display = 'block';
         }
     };
 
@@ -47,7 +40,7 @@ const Counter = () => {
         myShakeEvent.start();
 
         const handleShake = () => {
-            if(count < maxEnergy){
+            if (count < maxEnergy) {
                 setCount((prevCount) => prevCount + 1);
             }
         };
@@ -60,45 +53,45 @@ const Counter = () => {
         };
     };
 
-    const loadTelegramWidget = () => {
-        const script = document.createElement('script');
-        script.src = 'https://telegram.org/js/telegram-widget.js?19';
-        script.async = true;
-        script.setAttribute('data-telegram-login', 'shakeTongamebot'); // Replace with your bot's username
-        script.setAttribute('data-size', 'large');
-        script.setAttribute('data-auth-url', 'fendy-kocok.vercel.app/api/auth/telegram'); // Replace with your auth endpoint
-        script.setAttribute('data-request-access', 'write');
-        document.getElementById('telegram-login').appendChild(script);
+    const handleShake = () => {
+        if (count < maxEnergy) {
+            setCount((prevCount) => prevCount + 1);
+        }
     };
 
     return (
         <>
-        <Header curEnergy={maxEnergy - count} maxEnergy={maxEnergy}/>
-        <div className={`h-[calc(100vh-4rem)] flex items-center`}>
-            <div className=''>
-                {!permissionGranted && (
-                    <button
-                        className='bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded mb-4'
-                        onClick={checkMotionPermission}
-                    >
-                        Allow Device Motion
-                    </button>
-                )}
+            <Header curEnergy={maxEnergy - count} maxEnergy={maxEnergy} />
+            <div className={`h-[calc(100vh-4rem)] flex items-center`}>
+                <div className=''>
+                    {!permissionGranted && (
+                        <button
+                            className='bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded mb-4'
+                            onClick={checkMotionPermission}
+                        >
+                            Allow Device Motion
+                        </button>
+                    )}
 
-                <div className='w-full text-center'>
-                    <button onClick={handleClick}>klik</button>
-                    <h1 className='w-full font-bold text-2xl'>Shake to Increase Count</h1>
-                    <p>Shake your phone to increase the count:</p>
-                    <h2 className='mt-2 text-4xl'>{count}</h2>
-                </div>
-
-                    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-                        {/* <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={"width: 45%;"}> 45%</div> */}
+                    <div className='w-full text-center'>
+                        <h1 className='w-full font-bold text-2xl'>Shake to Increase Count</h1>
+                        <p>Shake your phone to increase the count:</p>
+                        <h2 className='mt-2 text-4xl'>{count}</h2>
+                        <button
+                            onClick={handleShake}
+                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'
+                        >
+                            Klik to Shake
+                        </button>
                     </div>
-                <Image className="mx-auto mt-5" src={logo} alt="Shake" width={450} height={450} />
+
+                    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700 mt-5">
+                        {/* <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${(count / maxEnergy) * 100}%` }}> {`${(count / maxEnergy) * 100}%`} </div> */}
+                    </div>
+
+                    <Image className="mx-auto mt-5" src={logo} alt="Shake" width={450} height={450} />
+                </div>
             </div>
-            
-        </div>
         </>
     );
 };

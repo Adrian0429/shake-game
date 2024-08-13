b9a8c56c84f566ba0e519b5d5d7c83ca9b55c536
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-08-13 23:38:25 +0700

    profile page

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -4,8 +4,6 @@ import React, { useEffect, useState } from 'react'
 
 interface UserData {
   id: number;
-  first_name: string;
-  last_name?: string;
   username?: string;
   language_code: string;
   is_premium?: boolean;
@@ -21,15 +19,25 @@ export default function Profile() {
     }, []);
 
   return (
-    <div>
-      <ul>
-        <li>ID: {userData?.id}</li>
-        <li>First Name: {userData?.first_name}</li>
-        <li>Last Name: {userData?.last_name || "N/A"}</li>
+    <div className="h-[calc(100vh-4.5rem)] w-full border p-4">
+      <h1 className="text-H2 w-full text-center">Profile User</h1>
+      <ul className="text-S2 mt-5">
+        <li>User ID: {userData?.id}</li>
         <li>Username: {userData?.username || "N/A"}</li>
         <li>Language Code: {userData?.language_code}</li>
-        <li>Is Premium: {userData?.is_premium ? "Yes" : "No"}</li>
       </ul>
+
+      <h2 className="text-H3 mt-10">Referral Code</h2>
+      <div className="w-full mt-2 bg-slate-900/80 flex px-4 justify-between items-center rounded-md border-2 border-dashed border-slate-700 h-16">
+        <p>PPHOOHK</p>
+        <button className="bg-slate-950 px-5 py-3 rounded-md">Copy</button>
+      </div>
+
+      <h2 className="text-H3 mt-10">Redeem Referral Code</h2>
+      <div className="flex mt-2 flex-row h-16 bg-white rounded-lg items-center">
+        <input type="text" className="w-full h-full rounded-l-lg text-black px-4" />
+        <button className="bg-primary text-black rounded-r-lg px-5 h-full">Redeem</button>
+      </div>
     </div>
   );
 }

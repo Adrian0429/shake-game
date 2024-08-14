30f4e4dbef861216fabe84350527188ec201029d
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-08-14 14:03:30 +0700

    feat: task cards buttble

--- src/app/(pages)/tasks/page.tsx
+++ src/app/(pages)/tasks/page.tsx
@@ -1,13 +1,12 @@
-"use client"
 import { useEffect, useState, useRef } from "react";
-import FooterNav from "../../components/Navigation/Footer";
+import Link from "next/link";
 
 const tasks = [
   {
     id: 1,
     title: "Task 1",
     description: "Follow Instagram",
-    link: "https://instagram.com/_adriankaruna",
+    link: "https://instagram.com/_adriankaruna/",
     completed: false,
   },
   {
@@ -27,18 +26,20 @@ const tasks = [
 const TaskPage = () => {
   return (
     <div className="w-full h-full py-20 ">
-      <div className="flex flex-col gap-y-4">
-        <h2>Daily Tasks</h2>
+      <div className="flex flex-col gap-y-4 items-center">
+        <h2 className="text-H2">Daily Tasks</h2>
         
         {tasks.map((task) => { 
           return (
             <div key={task.id} className="w-[80%]">
               <h3 className="text-H4">{task.title}</h3>
-              <div className="w-full bg-blue-300">
+              <div className="w-full flex flex-row justify-between bg-blue-300 p-4 rounded-lg">
                 <p>{task.description}</p>
-                <a href={task.link} target="_blank">
-                  Go to task
-                </a>
+                {task.link && (
+                  <Link href={task.link} target="_blank">
+                    GO
+                  </Link>
+                )}
               </div>
             </div>
           );

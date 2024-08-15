45c8d2b6150916d06ab62ec3cfa680bcc70dd800
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-08-15 15:50:14 +0700

    mobile only allow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,36 @@ import WebApp from "@twa-dev/sdk";
 import { useEffect, useState } from "react";
 import Header from "./components/Navigation/Header";
 
-
-
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
 
-    
+  useEffect(() => {
+    const isMobileDevice =
+      /Android|iPhone|iPad|iPod|webOS|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+
+    if (!isMobileDevice) {
+      alert(
+        "This application is designed for mobile devices. Some features may not work as expected."
+      );
+    } else {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <div className="h-[calc(100vh-4.5rem)]">
+      {isMobile ? (
         <Counter />
+      ) : (
+        <div className="flex justify-center items-center h-full">
+          <p className="text-center">
+            Please access this application on a mobile device for the best
+            experience.
+          </p>
+        </div>
+      )}
     </div>
   );
 }

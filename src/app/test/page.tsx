0b055530b7cb4967239e881bbfde45ac312a1801
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-09-14 23:01:31 +0700

    use client in testing page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,26 +1,26 @@
-"use client";
-import React from 'react'
-import ModalAllowComponent from '../components/Modal/ModalAllow';
+// "use client";
+// import React from 'react'
+// import ModalAllowComponent from '../components/Modal/ModalAllow';
 
-const page = () => {
-    const [isModalOpen, setIsModalOpen] = React.useState(true);
+// const page = () => {
+//     const [isModalOpen, setIsModalOpen] = React.useState(true);
 
-  const checkMotionPermission = async () => {
-    alert("Permission granted");
-    setIsModalOpen(false);
-  };
+//   const checkMotionPermission = async () => {
+//     alert("Permission granted");
+//     setIsModalOpen(false);
+//   };
   
 
-  return (
-    <div className="h-screen w-screen">
-      <ModalAllowComponent
-        username={"adrian"}
-        daily_count={5}
-        onAllowPermission={checkMotionPermission}
-        isOpen={isModalOpen}
-      />
-    </div>
-  );
-}
+//   return (
+//     <div className="h-screen w-screen">
+//       <ModalAllowComponent
+//         username={"adrian"}
+//         daily_count={5}
+//         onAllowPermission={checkMotionPermission}
+//         isOpen={isModalOpen}
+//       />
+//     </div>
+//   );
+// }
 
-export default page
+// export default page

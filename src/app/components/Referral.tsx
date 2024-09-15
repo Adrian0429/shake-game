69f119bc4bd7fa79257e0cd006236befedf4c141
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-09-15 21:37:03 +0700

    spacing in referral page

--- src/app/components/Referral.tsx
+++ src/app/components/Referral.tsx
@@ -83,7 +83,7 @@ const Referrals = ({userId} : props) => {
       <Header text="Shake Project" />
       <div className="h-[calc(100vh-9rem)] w-full flex flex-col items-center overflow-y-scroll">
         <Image
-          className="w-[40%] h-auto"
+          className="w-[40%] h-auto my-2"
           src={shake}
           alt=""
           objectFit="fit"
@@ -96,7 +96,7 @@ const Referrals = ({userId} : props) => {
           <p className="text-B3">Invite More, get even more bonuses!</p>
         </div>
 
-        <div className="my-3 rounded-lg bg-[#232328] flex flex-col w-[90%] space-y-5 py-6 justify-center items-center text-white">
+        <div className="my-4 rounded-lg bg-[#232328] flex flex-col w-[90%] space-y-5 py-6 justify-center items-center text-white">
           <h2>{referralsResponse.referralCoins ?? 0} Shake Points</h2>
           <div
             className="w-24 h-8 bg-[#FFD518]  cursor-pointer select-none
@@ -116,13 +116,13 @@ const Referrals = ({userId} : props) => {
           <p>Get a rewards play pass for each fren.</p>
         </div>
 
-        <div className="w-[90%] text-white">
+        <div className="w-[90%] text-white mt-2">
           <h1>List of your friends</h1>
           <div className="w-full">
             {referralsResponse.data.map((item, index) => (
               <div
                 key={index}
-                className="flex my-2 flex-row justify-between h-16 bg-[#232328] rounded-lg px-5 py-1 items-center"
+                className="flex my-3 flex-row justify-between h-16 bg-[#232328] rounded-lg px-5 py-1 items-center"
               >
                 <div className="flex flex-row w-full space-x-3 items-center">
                   <Image

6a93611956ee1b7c547436b579509533d6ed7816
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-09-18 22:52:06 +0700

    modal permission

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -17,15 +17,15 @@ function ModalPermission({
 
   return (
     <dialog className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto flex justify-center items-center">
-      <div className="relative flex flex-col items-center mx-auto py-4 px-6 bg-[#232328] h-[240px] w-[300px] rounded-lg z-100">
+      <div className="relative flex flex-col items-center mx-auto py-4 px-6 bg-[#232328] min-h-[240px] w-[300px] rounded-lg z-100">
         <h1 className="text-2xl font-bold text-center text-white">
           Welcome {username}
         </h1>
-        <h1 className="text-lg font-bold mt-4 text-center text-white">
+        <h1 className="text-lg font-bold mt-2 text-center text-white">
           Please Allow Access to your device shake sensor by clicking this button below
         </h1>
         <button
-          className="w-[80%] mt-2 h-12 bg-[#D5FF18] cursor-pointer select-none
+          className="w-[80%] mt-4 h-12 bg-[#D5FF18] cursor-pointer select-none
             active:translate-y-2 active:[box-shadow:0_0px_0_0_#ABC340,0_0px_0_0_#ffffff]
             active:border-b-[0px] transition-all duration-150 [box-shadow:0_2px_0_0_#ABC340,0_4px_0_0_#ffffff]
             rounded-full border-[1px] border-[#D5FF18] mb-3"

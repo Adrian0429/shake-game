244e02e557d2477670292e7d26a0e81890596bee
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-09-28 16:04:18 +0700

    change audio

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -47,20 +47,23 @@ const Counter = ({ count, energy, handleshake }: CounterProps) => {
 
       return () => clearTimeout(timer);
     }
+
     setLastCount(count);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [count, lastCount]);
 
   useEffect(() => {
     if (energy.current < 300) {
       setState("tired");
     } else if (state != "tired" && energy.current >= 300) {
-      setState("normal"); 
+      setState("normal"); // Reset to "normal" if energy is restored
     }
   }, [energy, state]);
 
+  // Update the GIF URL based on the current state
   useEffect(() => {
     setGifUrl(gifUrls[state]);
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state]);
 
   return (
@@ -92,11 +95,12 @@ const Counter = ({ count, energy, handleshake }: CounterProps) => {
           <div id="frenzybar" className="w-[80%] bg-gray-200 rounded-full mt-5">
             <div
               className="bg-[#E0FD60] text-xs font-medium text-brand-100 text-center p-2 leading-none rounded-full"
-              style={{ width: `${(energy.current / 2000) * 100}%` }}
+              style={{ width: `${(energy.current / 500) * 100}%` }}
             ></div>
           </div>
 
           <div
+
             className="w-[70%] my-4 h-16 bg-[#D5FF18] cursor-pointer select-none
       active:translate-y-1 active:[box-shadow:0_0px_0_0_#ABC340,0_0px_0_0_#ffffff]
       active:border-b-[0px] transition-all duration-150 [box-shadow:0_1.5px_0_0_#ABC340,0_4px_0_0_#ffffff]

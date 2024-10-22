5712b08bbaaa424a2d72d5db296dbf1365b2b9fc
Adrian Karuna Soetikno <royankaruna@gmail.com>
2024-10-22 21:08:10 +0700

    admin

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,120 @@
+"use client";
+import { FormProvider, useForm } from "react-hook-form";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { setCookie } from "nookies";
+
+type LoginRequest = {
+    email: string;
+    password: string;
+};
+
+type LoginResponse = {
+  data: {
+    role: string;
+    token: string;
+  };
+  message: string;
+  status: boolean;
+};
+
+const LoginAdmin = () => {
+    const router = useRouter()
+    const methods = useForm<LoginRequest>({
+        mode: "onChange",
+    });
+
+    const { handleSubmit } = methods;
+
+    const onSubmit = async (data: LoginRequest) => {
+        try {
+            const response = await axios.post<LoginResponse>(
+              "https://api2.fingo.co.id/api/user/login",
+              data,
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+
+            if(response.data.status == true){
+                setCookie(null, "adminJwt", response.data.data.token, {
+                  maxAge: 3 * 60 * 60,
+                  path: "/",
+                });
+                router.push('/admin')
+            }   
+        } catch (error) {
+            console.error('There was a problem with the login request:', error);
+        }
+    };
+
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div className="flex h-screen mx-4">
+            <FormProvider {...methods}>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="flex flex-col justify-center md:w-[60%] lg:w-[40%] xl:w-[30%] m-auto p-10 md:p-12 lg:p-16 border rounded-lg shadow-md space-y-8"
+                >
+                    <div className="space-y-5">
+                        <h3 className="text-H2 text-center">
+                            Masuk dengan email atau ID Anda
+                        </h3>
+                        <p className="text-B2 text-textGray text-center">
+                            Masukkan email dan kata sandi yang telah terdaftar
+                        </p>
+                    </div>
+                    
+                    <div className="space-y-4">
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                type="email"
+                                {...methods.register("email", { required: true })}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                                Password
+                            </label>
+                            <div className="relative">
+                                <input
+                                    id="password"
+                                    type={showPassword ? "text" : "password"}
+                                    {...methods.register("password", { required: true })}
+                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                                <button
+                                    type="button"
+                                    onClick={togglePasswordVisibility}
+                                    className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+                                >
+                                    {showPassword ? "Hide" : "Show"}
+                                </button>
+                            </div>
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </FormProvider>
+        </div>
+    );
+}
+
+export default LoginAdmin;
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import OffCanvasTask from "@/app/components/offcanvas/OffCanvasTask";
+import { LuYoutube } from "react-icons/lu";
+import { parseCookies } from "nookies";
+import { useRouter } from "next/navigation";
+
+type Task = {
+  id: number;
+  title: string;
+  description: string;
+  reward: number;
+  link: string;
+  code: string;
+  video: string;
+};
+
+const Page = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  // const [isOffcanvasVisible, setIsOffcanvasVisible] = useState(false);
+  // const [selectedTask, setSelectedTask] = useState<Task | null>(null); 
+
+  const router = useRouter();
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const cookies = parseCookies();
+        if(!cookies.adminJwt) {
+          router.push('/admin/login')
+        };
+        const response = await axios.get("https://api2.fingo.co.id/api/task", {
+          headers: {
+            Authorization: `Bearer ${cookies.adminJwt}`,
+          },
+        });
+        setTasks(response.data.data);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+    fetchTasks();
+  }, [router]);
+
+  // const toggleOffcanvas = (task:Task) => {
+  //   setSelectedTask(task);
+  //   setIsOffcanvasVisible(!isOffcanvasVisible);
+  // };
+
+  return (
+    <div className="flex h-screen w-screen">
+      <div className="flex flex-col h-[calc(100vh-4.5rem)] w-full items-center">
+        <div className="w-[90%] h-[calc(100vh-10rem)] overflow-y-scroll mt-5 space-y-3">
+          {tasks.map((task, index) => (
+            <div
+              key={index}
+              className="flex flex-row justify-between h-16 bg-[#232328] rounded-full px-5 py-1 items-center"
+            >
+              <div className="flex flex-row w-full space-x-3">
+                <div className="flex flex-col text-white">
+                  <p>{task.title}</p>
+                  <p>+ {task.reward} Coins</p>
+                </div>
+              </div>
+
+              <div
+                // onClick={() => toggleOffcanvas(task)}
+                className="w-24 h-8 bg-[#D5FF18] cursor-pointer select-none
+                  active:translate-y-2  active:[box-shadow:0_0px_0_0_#ABC340,0_0px_0_0_#ffffff]
+                  active:border-b-[0px]
+                  transition-all duration-150 [box-shadow:0_5px_0_0_#ABC340,0_8px_0_0_#ffffff]
+                  rounded-full  border-[1px] border-[#D5FF18] mb-3"
+              >
+                <LuYoutube className="w-full h-full text-black" />
+              </div>
+              {/* {selectedTask && (
+                <OffCanvasTask
+                  userId={123123}
+                  task={selectedTask}
+                  isVisible={isOffcanvasVisible}
+                  onClose={() => setIsOffcanvasVisible(false)} 
+                />
+              )} */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
